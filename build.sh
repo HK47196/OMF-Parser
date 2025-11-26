2783c7c8452af3ea7d46f046ef95b6795d95ee54
HK47196 <7722833+HK47196@users.noreply.github.com>
2025-11-26 07:28:46 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,6 +15,35 @@ PYTHONPATH=src uv run python -m nuitka \
     --output-filename="$BINARY_NAME" \
     --include-package=omf_parser \
     --assume-yes-for-downloads \
+    --nofollow-import-to=pydantic.deprecated.* \
+    --nofollow-import-to=pydantic.v1.* \
+    --nofollow-import-to=mypy \
+    --nofollow-import-to=mypy.* \
+    --nofollow-import-to=mypy_extensions \
+    --nofollow-import-to=pytest \
+    --nofollow-import-to=_pyrepl \
+    --nofollow-import-to=_pyrepl.* \
+    --nofollow-import-to=pdb \
+    --nofollow-import-to=bdb \
+    --nofollow-import-to=trace \
+    --nofollow-import-to=tracemalloc \
+    --nofollow-import-to=pstats \
+    --nofollow-import-to=timeit \
+    --nofollow-import-to=ftplib \
+    --nofollow-import-to=imaplib \
+    --nofollow-import-to=poplib \
+    --nofollow-import-to=socketserver \
+    --nofollow-import-to=symtable \
+    --nofollow-import-to=pyclbr \
+    --nofollow-import-to=py_compile \
+    --nofollow-import-to=modulefinder \
+    --nofollow-import-to=webbrowser \
+    --nofollow-import-to=curses \
+    --nofollow-import-to=curses.* \
+    --nofollow-import-to=__hello__ \
+    --nofollow-import-to=__phello__ \
+    --nofollow-import-to=__phello__.* \
+    --nofollow-import-to=*.tests \
     --deployment \
     build_entry.py
 
